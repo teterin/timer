9018779d8a620669f38168486cef1b11af0534a5
Pavel Teterin <teterin.pavel@gmail.com>
2019-09-05 19:10:53 +0200

    Simple timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0, isCountDown: false };
+  }
 
-export default App;
+  changeTime = ev => {
+    this.setState({ counter: +ev.target.value });
+  }
+
+  start = () => {
+    if (this.state.counter > 0) {
+      this.setState({ isCountDown: true }, this.delayedCountDown);
+    }
+  }
+
+  delayedCountDown = () => {
+    this.timerId = setTimeout(this.countDown, 1000);
+  }
+
+  countDown = () => {
+    const newVal = this.state.counter - 1;
+    if (newVal === 0) {
+      this.setState({ isCountDown: false, counter: 0 });
+    } else {
+      this.setState({ counter: newVal }, this.delayedCountDown);
+    }
+  }
+
+  stop = () => {
+    clearTimeout(this.timerId);
+    this.setState({ isCountDown: false, counter: 0 });
+  }
+
+  render() {
+    const { counter, isCountDown } = this.state;
+
+    return (<div className="container">
+      {!isCountDown && <div>
+        <div>Welcome to timer. Set seconds and click start</div>
+        <div><input value={counter} onChange={this.changeTime} /></div>
+        <div><button onClick={this.start}>Start</button></div>
+      </div>}
+      {isCountDown && <div>
+        <div>{counter}</div>
+        <div><button onClick={this.stop}>Stop</button></div>
+      </div>}
+    </div>);
+  }
+}
